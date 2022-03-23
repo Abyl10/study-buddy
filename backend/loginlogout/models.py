5f58171b19268afcf5d15eb4452a62261c854d2f
Meiirlan Suiirkhanov <meyrlansuirkhanov@gmail.com>
2022-03-23 21:18:46 +0600

    backend feat: initial commit

---
+++ backend/loginlogout/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, null=False)
+    password = models.CharField(max_length=50)
+    ifLogged = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} -{}".format(self.username, self.email)
